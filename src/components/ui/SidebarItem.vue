ea564ca2b272e8bf5573b74c21de861f15cfd260
trinidj <jm.trinidad199@gmail.com>
2025-08-10 13:32:17 -0400

    refactor: improve sidebar item styling and layout

--- src/components/ui/SidebarItem.vue
+++ src/components/ui/SidebarItem.vue
@@ -9,14 +9,14 @@
 </script>
 
 <template>
-  <li class="px-4 py-2 hover:bg-gray-100 rounded-md transition-colors">
+  <li class="p-2 hover:bg-gray-200 rounded-md transition-colors">
     <RouterLink
       :to="props.path"
       class="flex items-center gap-3 text-gray-700 hover:text-purple-600 font-medium"
       active-class="text-purple-600"
     >
-      <component :is="props.icon" class="text-lg" />
-      {{ props.name }}
+      <component :is="props.icon" />
+      <p class="text-md">{{ props.name }}</p>
     </RouterLink>
   </li>
 </template>
