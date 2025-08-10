798650d51e3f7c98e8bc4a471fd0d09e46aaf40d
trinidj <jm.trinidad199@gmail.com>
2025-08-10 13:31:38 -0400

    feat: add navigation items and collapsible sidebar functionality

--- src/components/layout/Sidebar.vue
+++ src/components/layout/Sidebar.vue
@@ -1,36 +1,75 @@
 <script setup>
+  import { ref } from 'vue';
   import SidebarItem from '@/components/ui/SidebarItem.vue';
-  import { ArrowLeftToLine, LayoutDashboard, CreditCard } from 'lucide-vue-next';
+  import { ArrowLeftToLine, LayoutDashboard, CreditCard, ChartArea, User, Settings, Banknote, Wallet } from 'lucide-vue-next';
 
   const navItems = [
     { name: 'Dashboard', path: '/dashboard', icon: LayoutDashboard },
     { name: 'Transactions', path: '/transactions', icon: CreditCard },
+    { name: 'Budgets', path: '/budgets', icon: Banknote },
+    { name: 'Reports', path: '/analytics', icon: ChartArea },
+    { name: 'Wallets', path: '/wallets', icon: Wallet }
   ]
+
+  const accountItems = [
+    { name: 'Profile', path: '/profile', icon: User },
+    { name: 'Settings', path: '/settings', icon: Settings },
+  ]
+
+  const sidebarOpen = ref(true);
+
+  const toggleSidebar = () => {
+    console.log('Click!');
+    sidebarOpen.value = !sidebarOpen.value;
+  }
+
 </script>
 
 <template>
   <aside class="h-screen">
-    <nav class="h-full flex flex-col bg-white w-70 border-r-1 shadow-2xl border-x-purple-400">
-      <div class="flex flex-row items-center justify-between p-6">
-        <div class="flex flex-col gap-1">
+    <nav 
+      class="h-full flex flex-col justify-between bg-white border-r-1 border-r-purple-400 shadow-2xl transition-all w-60"
+    >
+      <div class="flex flex-row p-5 items-center justify-between">
+        <div :class="['overflow-hidden transition-all whitespace-nowrap', sidebarOpen ? 'w-40' : 'w-0']">
           <h1 class="font-medium">Expense Tracker</h1>
           <p class="text-xs">Keep Track of your Expenses</p>
         </div>
-        <button class="p-2 rounded transition duration-300 ease hover:cursor-pointer hover:bg-gray-500/20 hover:">
+        <button 
+          @click="toggleSidebar" 
+          class="flex items-center justify-center p-2 rounded transition duration-300 ease hover:cursor-pointer hover:bg-gray-500/20"
+        >
           <ArrowLeftToLine
             :size="16"
+            :class="['transition-transform duration-100', sidebarOpen ? '' : 'rotate-180']"
           />
         </button>
       </div>
 
-      <ul class="flex flex-col gap-2 px-4 py-8">
-        <SidebarItem 
-          v-for="item in navItems"
-          :name="item.name"
-          :path="item.path"
-          :icon="item.icon"
-        />
-      </ul>
+      <div>
+        <ul class="flex flex-col gap-5 p-5">
+          <SidebarItem 
+            v-for="item in navItems"
+            :key="item.path"
+            :name="item.name"
+            :path="item.path"
+            :icon="item.icon"
+          />
+        </ul>
+      </div>
+      
+
+      <div class="flex flex-col mt-auto">
+        <ul class="flex flex-col gap-2 p-5">
+          <SidebarItem 
+            v-for="item in accountItems"
+            :key="item.path"
+            :name="item.name"
+            :path="item.path"
+            :icon="item.icon"
+          />
+        </ul>
+      </div>
     </nav>
   </aside>
 </template>
