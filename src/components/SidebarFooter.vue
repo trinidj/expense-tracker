907f8bd355afdbff50774f8c5761dba55412dc0b
trinidj <jm.trinidad199@gmail.com>
2025-08-10 21:18:22 -0400

    feat: add SidebarFooter component for account navigation

---
+++ src/components/SidebarFooter.vue
@@ -0,0 +1,29 @@
+<script setup>
+  import SidebarItem from '@/components/ui/SidebarItem.vue';
+
+  const props = defineProps({
+    accountItems: {
+      type: Array
+    },
+    isExtended: {
+      type: Boolean,
+      default: false
+    }
+  })
+</script>
+
+<template>
+  <div class="flex flex-col mt-auto">
+    <ul class="flex flex-col gap-2 p-5">
+      <SidebarItem 
+        v-for="item in accountItems"
+        :key="item.path"
+        :name="item.name"
+        :path="item.path"
+        :icon="item.icon"
+        :size="item.size"
+        :sidebar-open="isExtended"
+      />
+    </ul>
+  </div>
+</template>
