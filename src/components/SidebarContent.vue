ce4f8af89ccc55033a0894362d176579d81e4603
trinidj <jm.trinidad199@gmail.com>
2025-08-10 21:12:42 -0400

    feat: extract sidebar navigation into SidebarContent component

---
+++ src/components/SidebarContent.vue
@@ -0,0 +1,28 @@
+<script setup>
+  import SidebarItem from '@/components/ui/SidebarItem.vue';
+
+  const props = defineProps({
+    navItems: {
+      type: Array
+    },
+    isExtended: {
+      type: Boolean,
+      default: false
+    }
+  })
+</script>
+
+<template>
+  <div class="transition-all overflow-hidden">
+    <ul class="flex flex-col gap-5 p-5 transition-all overflow-hidden">
+      <SidebarItem 
+        v-for="item in navItems"
+        :key="item.path"
+        :name="item.name"
+        :path="item.path"
+        :icon="item.icon"
+        :sidebar-open="isExtended"
+      />
+    </ul>
+  </div>
+</template>
