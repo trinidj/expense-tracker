04ce7ea6ccd2a94209d7071b813c790542d523cf
trinidj <jm.trinidad199@gmail.com>
2025-08-11 00:28:31 -0400

    feat: add RouterView and gradient background styling to main app

--- src/App.vue
+++ src/App.vue
@@ -6,5 +6,24 @@
 <template>
   <Main>
     <Sidebar />
+    <RouterView id="page"/>
   </Main>
 </template>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap');
+
+  * {
+    font-family: 'Ubuntu', sans-serif;
+  }
+
+  #page {
+    background: linear-gradient(
+      to bottom,
+      #fbf1ff,  
+      #f8e7ff,
+      #f5deff,
+      #f2d3ff   
+    );
+  }
+</style>
