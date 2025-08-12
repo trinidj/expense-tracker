50316029832e9d1d87e8c19b5d3a10f2ad10d7e5
trinidj <jm.trinidad199@gmail.com>
2025-08-12 11:44:48 -0400

    feat: implement grid-based layout system for dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -4,8 +4,8 @@
 
 <template>
   <section class="flex-1">
-    <div class="p-10">
-      <div class="flex flex-row gap-20 p-5 text-gray-800">
+    <div class="grid grid-cols-[2fr_1fr] grid-rows-5 p-10 gap-4 md:gap-6 lg:gap-6">
+      <div class="flex gap-20 p-5 text-gray-800 bg-red-300 col-span-2">
         <div class="flex flex-col gap-2">
           <h1 class="flex items-center gap-2 text-lg">
             <BanknoteArrowUp 
@@ -26,6 +26,9 @@
           <h2 class="text-5xl">$500.00</h2>
         </div>
       </div>
+      
+      <div class="row-span-6 row-start-2 bg-red-300">3</div>
+      <div class="row-span-6 row-start-2 bg-red-300">4</div>
     </div>
   </section>
 </template>
