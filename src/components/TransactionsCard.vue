2fb5bb499413a2c3c71ec7edd0e345a515eb6ba5
trinidj <jm.trinidad199@gmail.com>
2025-08-14 18:02:05 -0400

    feat: add header styling and view all link to TransactionsCard

--- src/components/TransactionsCard.vue
+++ src/components/TransactionsCard.vue
@@ -1,9 +1,18 @@
-<script setup></script>
+<script setup>
+  import { RouterLink } from 'vue-router';
+</script>
 
 <template>
   <section class="bg-white/45 col-start-4 col-span-1 row-span-4 rounded-xl">
-    <header>
-      <h2>Recent Transactions</h2>
+    <header class="p-6 flex flex-row justify-between item-center">
+      <h2 class="text-balance font-medium">Recent Transactions</h2>
+
+      <RouterLink
+        :to="'/transactions'"
+        class="hover:underline"
+      >
+        <p>View All</p>
+      </RouterLink>
     </header>
 
     <div>
