5cc441cac146139b60d587abc94cd351d9d69b26
trinidj <jm.trinidad199@gmail.com>
2025-08-14 21:20:27 -0400

    feat: integrate PrimeVue with Aura theme configuration

--- src/main.js
+++ src/main.js
@@ -2,7 +2,15 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
 
 const app = createApp(App);
+
 app.use(router);
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  }
+});
 app.mount('#app');
