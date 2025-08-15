dfe21b89c319e2d8695ac00744a3725dc57583dc
trinidj <jm.trinidad199@gmail.com>
2025-08-15 13:54:41 -0400

    refactor: remove page styling and simplify App.vue template

--- src/App.vue
+++ src/App.vue
@@ -14,24 +14,6 @@
       @mouseenter="isExtended = true"
       @mouseleave="isExtended = false"
     />
-    <RouterView id="page"/>
+    <RouterView />
   </Main>
 </template>
-
-<style>
-  @import url('https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap');
-
-  * {
-    font-family: 'Ubuntu', sans-serif;
-  }
-
-  #page {
-    background: linear-gradient(
-      to bottom,
-      #fbf1ff,  
-      #f8e7ff,
-      #f5deff,
-      #f2d3ff   
-    );
-  }
-</style>
